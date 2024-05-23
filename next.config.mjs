7eb192251b5fb0f58faa765e223d334482a6b311
SHIV3710 <yshiv4573@gmail.com>
2024-05-23 23:34:53 +0530

    Cors Error

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,29 @@ const nextConfig = {
       {
         source: '/',
         destination: '/login',
-        permanent: false, 
+        permanent: false,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // Match all API routes
+        source: '/api/:path*',
+        headers: [
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: '*', // You can specify your allowed origins here
+          },
+          {
+            key: 'Access-Control-Allow-Methods',
+            value: 'GET,POST,PUT,DELETE,OPTIONS',
+          },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value: 'X-Requested-With, Content-Type, Authorization',
+          },
+        ],
       },
     ];
   },
